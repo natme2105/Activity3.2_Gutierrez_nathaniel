14d5f09fca2d5bec58deefd21139c3ae73585f5a
natme2105 <nathanielgutierrez70@gmail.com>
2023-11-07 22:49:33 +0800

    feat/add-column-price

--- hero.sql
+++ hero.sql
@@ -152,3 +152,9 @@ VALUES
     (8, 8, 108),  -- Fanny has Thunder Belt
     (9, 9, 109),  -- Leomord has Blade Armor
     (10, 10, 110);  -- Chou has Feather of Heaven
+
+    /*Add another column in the item table named item_price.
+	Set the column Price data type to Decimal with 2 decimal points.*/
+
+    ALTER TABLE Item 
+    ADD item_price DECIMAL(10, 2);
